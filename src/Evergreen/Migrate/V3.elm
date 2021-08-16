1cfa3b201ee0451b75b18d3597fbca8e79f75379
Stewart Murrie <stewart@murrie.org>
2021-08-16 11:04:59 -0700

    Preparing for v3

--- src/Evergreen/Migrate/V3.elm
+++ src/Evergreen/Migrate/V3.elm
@@ -1,14 +1,26 @@
 module Evergreen.Migrate.V3 exposing (..)
-import Evergreen.V3.QuestionTree as V3 exposing (Answer(..), QuestionTree)
-import Evergreen.V2.QuestionTree as V2 exposing (Answer(..), QuestionTree(..))
+
+import Evergreen.V2.QuestionTree as V2 exposing (Answer(..), QuestionTree)
 import Evergreen.V2.Types as Old
+import Evergreen.V3.QuestionTree as V3 exposing (Answer(..), QuestionTree(..))
 import Evergreen.V3.Types as New
 import Lamdera.Migrations exposing (..)
 
 
 frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
 frontendModel old =
-    ModelUnchanged
+    ModelMigrated
+        ( { key = old.key
+          , tree = upgradeTree old.tree
+          , currentNode = upgradeTree old.currentNode
+          , state = upgradeGameState old.state
+          , movieFieldText = old.movieFieldText
+          , questionFieldText = old.questionFieldText
+          , questionLog = old.questionLog
+          , movieCount = 0
+          }
+        , Cmd.none
+        )
 
 
 backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
@@ -19,6 +31,36 @@ backendModel old =
 frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
 frontendMsg old =
     MsgUnchanged
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgUnchanged
+
+
+upgradeAnswer : V2.Answer -> V3.Answer
+upgradeAnswer old =
+    case old of
+        V2.No ->
+            V3.No
+
+        V2.Yes ->
+            V3.Yes
+
+
+upgradeTree : V2.QuestionTree -> V3.QuestionTree
+upgradeTree old =
+    case old of
+        V2.Empty ->
+            V3.Empty
+
+        V2.Node v l r ->
+            V3.Node v (upgradeTree l) (upgradeTree r)
 
 
 toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
@@ -34,29 +76,29 @@ toBackend old =
             MsgMigrated ( New.GetMovieCount, Cmd.none )
 
 
-upgradeTree : V2.QuestionTree -> V3.QuestionTree
-upgradeTree old =
+upgradeGameState : Old.GameState -> New.GameState
+upgradeGameState old =
     case old of
-        V2.Empty ->
-            V3.Empty
+        Old.InLobby ->
+            New.InLobby
 
-        V2.Node v l r ->
-            V3.Node v (upgradeTree l) (upgradeTree r)
+        Old.Running ->
+            New.Running
 
-upgradeAnswer : V2.Answer -> V3.Answer
-upgradeAnswer old =
-    case old of
-        V2.No ->
-            V3.No
+        Old.Won ->
+            New.Won
 
-        V2.Yes ->
-            V3.Yes
+        Old.Lost ->
+            New.Lost
 
-backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
-backendMsg old =
-    MsgUnchanged
+        Old.GotMovie ->
+            New.GotMovie
 
+        Old.GotQuestion ->
+            New.GotQuestion
 
-toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
-toFrontend old =
-    MsgUnchanged
+        Old.MovieAdded ->
+            New.MovieAdded
+
+        Old.EditQuestion ->
+            New.EditQuestion
